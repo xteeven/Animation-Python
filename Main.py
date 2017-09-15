da6fce3654790323a68cf143351155a8b8335d9e
David Steeven Villa Salazar <david7246@gmail.com>
2017-09-15 20:20:30 -0300

    thr

--- Main.py
+++ Main.py
@@ -1,44 +1,22 @@
 import pygame
 from pygame.locals import *
-
+import numpy as np
 from OpenGL.GL import *
 from OpenGL.GLU import *
 from Auxs import OBJ
 
-
-verticies = (
-    (1, -1, -1),
-    (1, 1, -1),
-    (-1, 1, -1),
-    (-1, -1, -1),
-    (1, -1, 1),
-    (1, 1, 1),
-    (-1, -1, 1),
-    (-1, 1, 1)
-    )
-
-edges = (
-    (0,1),
-    (0,3),
-    (0,4),
-    (2,1),
-    (2,3),
-    (2,7),
-    (6,3),
-    (6,4),
-    (6,7),
-    (5,1),
-    (5,4),
-    (5,7)
-    )
+parametric = []
+for t in range(1000):
+    parametric.append((10*np.tan(t/np.pi)+np.sin(t / np.pi), (np.log(t+1/ np.pi)),2*np.cos(t / np.pi)))
 
 
-def Cube():
-    glBegin(GL_LINES)
-    for edge in edges:
-        for vertex in edge:
-            glVertex3fv(verticies[vertex])
+def drawCurve(Curve):
+    glBegin(GL_LINE_STRIP)
+    for t in range(len(Curve)):
+        glVertex3fv(Curve[t])
     glEnd()
+
+
 
 
 def main():
@@ -46,7 +24,7 @@ def main():
     display = (800,600)
     pygame.display.set_mode(display, DOUBLEBUF | OPENGL)
 
-    gluPerspective(45, (display[0]/display[1]), 0.1, 50.0)
+    gluPerspective(45, (display[0]/display[1]), 0.1, 500.0)
 
     glTranslatef(0.0,0.0, -10)
 
@@ -55,10 +33,17 @@ def main():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 quit()
+        mouse = pygame.mouse.get_rel()
 
-        glRotatef(5, 1, 0, 1)
+        if pygame.mouse.get_pressed()[0] & ((mouse[0] != 0) | (mouse[0] != 0)):
+            vel = np.power(mouse[0] ** 2 + mouse[1] ** 2, 1 / 2.0)
+            glRotatef(int(np.abs(vel)), mouse[0], 0, mouse[1])
+
+
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
-        Cube()
+        glTranslatef(0.0, 0.0, 0)
+        # Cube()
+        drawCurve(parametric)
         pygame.display.flip()
         pygame.time.wait(10)
 
