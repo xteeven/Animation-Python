aff22ef0c89330867716fb4a65ce1c25be0da77c
Steeven Villa <david7246@gmail.com>
2017-10-16 03:03:22 -0200

    d

--- IK.py
+++ IK.py
@@ -4,7 +4,6 @@ from OpenGL.GLU import *
 from Drawings import *
 import numpy as np
 from math import sin, cos, radians, degrees, atan
-import pyrenn as prn
 
 
 class Link:
@@ -118,6 +117,12 @@ def setup():
     pygame.init()
     display = (800, 600)
     pygame.display.set_mode(display, DOUBLEBUF | OPENGL)
+    # glEnable(GL_CULL_FACE)
+    # glEnable(GL_LIGHTING)
+    glColorMaterial(GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE)
+    glEnable(GL_COLOR_MATERIAL)
+    glEnable(GL_LIGHT0)
+    glLightfv(GL_LIGHT0, GL_POSITION, [10, 10, 10])
     glMatrixMode(GL_MODELVIEW)
     gluPerspective(45, (display[0]/display[1]), 0.1, 250.0)
     glTranslatef(0.0, 5, -30)
@@ -128,9 +133,9 @@ def main():
 
     setup()
     base = Link((1, 1, 1), (1, 0, 0))
-    link1 = Link((.5, .5, 3), (0, 1, 0))
-    link2 = Link((.5, .5, 2), (0, 0, 1))
-    link3 = Link((.5, .5, 1), (1, 0, 1))
+    link1 = Link((.2, .2, 3), (0, 1, 0))
+    link2 = Link((.2, .2, 2), (0, 0, 1))
+    link3 = Link((.2, .2, 1), (1, 0, 1))
 
     base.appendArm(link1)
     link1.appendArm(link2)
@@ -139,25 +144,6 @@ def main():
     robot = Arm(base, link1, link2, link3)
 
     point = [3, 3, 3]
-    P = []
-    Y = []
-    for i in range(100):
-        out = [np.random.randint(0, 360),
-               np.random.randint(-90, 90),
-               np.random.randint(-90, 90),
-               np.random.randint(-90, 90)]
-        robot.setRotation(out[0])
-        robot.setAngle(0, out[1], out[2], out[3])
-        robot.update()
-        P.append(robot.currentPos[0])
-        Y.append(out)
-    P = np.asarray(P).transpose()
-    Y = np.asarray(Y).transpose()
-    net = prn.CreateNN([3, 10, 5, 8, 4], dIn=[0], dIntern=[], dOut=[1])
-    net = prn.train_LM(P, Y, net, verbose=True, k_max=100, E_stop=1e-5)
-
-
-
 
 
     i = 0
@@ -171,21 +157,12 @@ def main():
         # Draw a grid
         drawGrid(0)
 
-        ytest = prn.NNOut(np.array(zip(point)), net)
-
-        robot.setRotation(ytest[0])
-
+        rotation = 90*(1-np.sign(point[1]))-degrees(atan(point[0]/point[1]))
+        robot.setRotation(rotation)
         robot.setAngle(0,
-                       ytest[1],
-                       ytest[2],
-                       ytest[3])
-
-        # robot.setRotation(0)
-        # robot.setAngle(0,
-        #                0,
-        #                0,
-        #                0)
-
+                       0,
+                       i,
+                       0)
 
         robot.update()
 
@@ -195,8 +172,7 @@ def main():
 
         drawCoord(point)
         drawCoord(robot.currentPos[0])
-
-        i += 1
+        i = i-1 if i < 360 else 0
 
         pygame.display.flip()
 
